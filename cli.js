bb63206a57a4b12cd2d6a5f4b94623b9289517e1
Dani de la Cruz <mail@danidev.es>
2019-02-03 22:03:19 +0100

    Show only headers

--- cli.js
+++ cli.js
@@ -5,16 +5,28 @@ const prompt = require("./lib/prompt");
 
 const [, , ...args] = process.argv;
 let url = args[0];
+let headers = args.includes("-H") || args.includes("--headers");
+
+const doFetch = () => {
+  fetch(url)
+    .then(processResponse)
+    .then(console.log)
+    .catch(console.error);
+};
+
+const processResponse = response => {
+  if (headers) {
+    return response.headers._headers;
+  }
+  return response.json();
+};
 
 if (!url) {
   prompt()
     .then(responses => {
       url = responses.url;
     })
-    .then(() => {
-      fetch(url)
-        .then(r => r.json())
-        .then(console.log)
-        .catch(console.error);
-    });
+    .then(doFetch);
+} else {
+  doFetch();
 }
