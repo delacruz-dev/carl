ee29fb7101f39cf60fd025e84ffdd554cc4d1810
Dani de la Cruz <mail@danidev.es>
2019-02-23 07:08:31 +0100

    Support urls without protocol

--- cli.js
+++ cli.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
+
+const protocolRegex = /http(s)?:\/\//gm;
+
 const prompt = require("./lib/prompt");
 const { processResponse } = require("./lib/response-processor");
 const { printResponse } = require("./lib/response-printer");
 
 const [, , ...args] = process.argv;
 let url = args[0];
+
+if (!protocolRegex.test(url)) {
+  url = `https://${url}`;
+}
+
 let headers = args.includes("-H") || args.includes("--headers");
 
 const doFetch = () => {
